4cac297e9b7f6127b812227a309078169c090ea2
ksascomm <ksasweb@jhu.edu>
2015-11-16 12:45:59 -0500

    removed slider from front-page

--- template-redesign-frontpage.php
+++ template-redesign-frontpage.php
@@ -42,47 +42,103 @@ Template Name: Redesign Front Page
 				'order' => 'DESC',
 				'posts_per_page' => '-1'));
 
+			$asmag_cover_story_query = new WP_Query(array(
+				'post_type' => 'page',
+				'volume' => $volume,
+				'post_parent' => $parent,
+				'page_id' => 6243,			
+				));
+
+			$exclude_ids = array( 6243 );
 			$asmag_features_query = new WP_Query(array(
 				'post_type' => 'page',
 				'volume' => $volume,
 				'post_parent' => $parent,
+				'post__not_in' => $exclude_ids,
 				'orderby' => 'menu_order',
 				'order' => 'ASC',
 				'posts_per_page' => '5'));			
 
 	?>
 
-<section class="features">
-<div class="slideshow-wrapper">
-  <div class="preloader"></div>
-	<ul class="slider" data-orbit data-options="animation: fade; animation_speed:1000; timer:false; timer_speed:10000; navigation_arrows:true; bullets:true; slide_number:false;">
-	<?php if ( $asmag_features_query->have_posts() ) : while ($asmag_features_query->have_posts()) : $asmag_features_query->the_post(); ?>
-	  <li>
-	  	<a href="<?php the_permalink();?>" title="<?php the_title(); ?>" class="field">
-	  	<div class="slide">
-		    <?php echo the_post_thumbnail('fullbleed', array('class'=>'no-margin no-lazy')); ?>
-		    <div class="slide-caption">
-				<h1><?php the_title(); ?></h1>
-				<p><?php if ( get_post_meta($post->ID, 'ecpt_tagline', true) ) { echo get_post_meta($post->ID, 'ecpt_tagline', true); } else { the_excerpt(); } ?></p>
-				<p>	
-					<?php
-						$author_id = $post->post_author;
-						$author = get_the_author();
-						if ( $author_id != 1 ) {
-							//dont show byline for "ksascomm"
-						  echo 'By&nbsp;' . $author ;
-						}
-						?>
-				</p>		
-		    </div>
-		 </div>
-	    </a>
-	  </li>
-	  <?php endwhile; wp_reset_postdata(); ?>	
-	</ul>
-	</div>
+<?php if ( $asmag_cover_story_query->have_posts() ) : ?>
+<section class="cover-story home">
+ <?php while ($asmag_cover_story_query->have_posts()) : $asmag_cover_story_query->the_post(); ?>
+ 	<?php $image = wp_get_attachment_url( get_post_thumbnail_id($post->ID));
+$img_size_lg = 'large-hero'; 
+$img_size_md = 'medium-hero';
+$img_size_sm = 'mobile-hero';
+ 
+$feat_img_id = get_post_thumbnail_id();
+ 
+/* Use ID to get the attachment object */
+$xlg_hero_array = wp_get_attachment_image_src( $feat_img_id, 'xtra-large-hero', true ); //X-Large Hero
+$lg_hero_array = wp_get_attachment_image_src( $feat_img_id, 'large-hero', true ); //Large Hero
+$md_hero_array = wp_get_attachment_image_src( $feat_img_id, 'medium-hero', true ); // Medium Hero
+$sm_hero_array = wp_get_attachment_image_src( $feat_img_id, 'mobile-hero', true ); // Mobile Hero
+ 
+/* Grab the url from the attachment object */
+$hero_xlg = $xlg_hero_array[0]; //X-Large Hero
+$hero_lg = $lg_hero_array[0]; //Large Hero
+$hero_md = $md_hero_array[0]; // Medium Hero
+$hero_sm = $sm_hero_array[0]; // Mobile Hero
+
+ ?>
+ <div class="intro-hero" data-interchange="[<?php echo $hero_xlg; ?>, (default)], [<?php echo $hero_sm; ?>, (small)], [<?php echo $hero_md; ?>, (medium)], [<?php echo $hero_lg; ?>, (large)] [<?php echo $hero_xlg; ?>, (xlarge)]" style="margin-top: -20px;">
+ 	 		<div class="row">
+                <div class="small-7 small-offset-5 medium-6 medium-offset-6 large-7 large-offset-6 columns">
+                    <div class="post-heading">
+                    	<a href="<?php the_permalink();?>" title="<?php the_title(); ?>" class="field">
+                        <h2><?php the_title(); ?></h2>
+							<div class="show-for-large-up">
+		                        <?php if ( get_post_meta($post->ID, 'ecpt_tagline', true) ) : ?> 
+		                        	<div id="tagline">
+		                        		<h4>
+		                        			<?php echo get_post_meta($post->ID, 'ecpt_tagline', true); ?>
+		                        		</h4>
+		                        	</div>
+		                        <?php endif; ?>
+		                    </div>
+                    	</a>
+                    </div>
+                </div>
+        	</div> 	
+</div>
+			<div class="hide-for-large-up">
+				<div class="row">
+				<div class="small-12 columns">
+	            <?php if ( get_post_meta($post->ID, 'ecpt_tagline', true) ) : ?> 
+	            	<div id="tagline">
+	            		<h5>
+	            			<?php echo get_post_meta($post->ID, 'ecpt_tagline', true); ?>
+	            		</h5>
+	            	</div>
+	            <?php endif; ?>
+	            </div>
+	            </div>
+		    </div>  
+<?php endwhile; ?>
 </section>
 <?php endif; ?>
+<section class="features home">
+	<div class="row section-header">
+		<h2 class="white">Features</h2>
+	</div>
+	<div class="row">
+		<?php if ( $asmag_features_query->have_posts() ) : ?>	
+			<ul class="small-block-grid-1 medium-block-grid-3">
+				<?php while ($asmag_features_query->have_posts()) : $asmag_features_query->the_post(); ?>
+				<li class="features item">
+					<a href="<?php the_permalink();?>" title="<?php the_title(); ?>" class="field"><?php echo the_post_thumbnail('filterthumbbig', array('class'=>'no-margin home-img img-responsive')); ?>
+					<h5><?php the_title(); ?></h5>
+					<p><?php echo get_post_meta($post->ID, 'ecpt_tagline', true); ?></p>
+					</a>
+				</li>
+				<?php endwhile; wp_reset_postdata(); ?>	
+			</ul>
+			<?php endif; ?>
+	</div>
+</section>
 
 								
 <section class="news home">
